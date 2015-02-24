741bed295a5410f9c2c4a912d4eddcfb967af108
Michael Cleyn <michael.cleyn@shopify.com>
2015-02-23 19:29:47 -0500

    fix calculator add ruby app

--- Week3Day2/Calculator/app.rb
+++ Week3Day2/Calculator/app.rb
@@ -1,6 +1,14 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-get ('/') do
+get ('/add') do
+ 	erb :calculator, locals: {answer: nil }
+end
 
+post ('/add') do
+	num1 = params[:num1].to_i
+ 	num2 = params[:num1].to_i
+
+ 	answer = num1 + num2
+ 	erb :calculator, locals: {answer: answer}
 end
