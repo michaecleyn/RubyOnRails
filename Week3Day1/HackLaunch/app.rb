6506574f86fa749dc8de993bc67b37ea738c2259
Michael Cleyn <michael.cleyn@shopify.com>
2015-02-28 17:52:38 -0500

    adding hacklaunch app

--- Week3Day1/HackLaunch/app.rb
+++ Week3Day1/HackLaunch/app.rb
@@ -1,10 +1,22 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+# in rails you don't have to write erb :form, because it will automatically
+# render this file and search it out in the view.
+# you can only render one file in a route.
+
+#post requests pages always happen after an actiong. Like a thank you for signing in page.
+#post requests don't even work if you don't send info or submit anything
+
 get ('/') do
  erb :form
 end
 
+CONTACTS = []
+
 post ('/submitted') do
- erb :thanks
+	thename = params[:name]
+	theemail = params[:email]
+	CONTACTS << theemail
+	erb :thanks, locals: {fries: thename, burger: theemail}
 end
