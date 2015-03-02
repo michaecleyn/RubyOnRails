b5df9f21fbc4cbaa76161f0a6e61f41b6a7bc2f1
Michael Cleyn <michael.cleyn@shopify.com>
2015-03-01 20:16:58 -0500

    added li loop to submit

--- Week3Day1/Repeat/app.rb
+++ Week3Day1/Repeat/app.rb
@@ -5,8 +5,11 @@ get '/' do
   erb :form
 end
 
+EMAILS = []
+
 post '/submit' do
 	name = params[:name]
 	email = params[:email]
+	EMAILS << name + " " + email
 	erb :submit, locals: {x: name, y: email}
 end
